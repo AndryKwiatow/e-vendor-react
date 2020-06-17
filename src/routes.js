9c2a58bdf9f01d5149b762ff6454388f767e9355
Andres Ramos <andresramos053@gmail.com>
2020-06-17 00:45:58 -0500

    Add new options in menu

--- src/routes.js
+++ src/routes.js
@@ -28,59 +28,87 @@ var dashRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    icon: "design_app",
+    icon: "business_chart-pie-36",
     component: Dashboard,
     layout: "/admin"
   },
   {
-    path: "/icons",
-    name: "Icons",
-    icon: "design_image",
+    path: "/invoice",
+    name: "Facturar",
+    icon: "files_paper",
+    component: Dashboard,
+    layout: "/admin"  
+  },
+  {
+    path: "/clients",
+    name: "Clientes",
+    icon: "users_single-02",
     component: Icons,
     layout: "/admin"
   },
   {
-    path: "/maps",
-    name: "Maps",
-    icon: "location_map-big",
-    component: Maps,
+    path: "/products",
+    name: "Productos",
+    icon: "shopping_tag-content",
+    component: Icons,
     layout: "/admin"
   },
   {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "ui-1_bell-53",
-    component: Notifications,
-    layout: "/admin"
-  },
-  {
-    path: "/user-page",
-    name: "User Profile",
-    icon: "users_single-02",
-    component: UserPage,
-    layout: "/admin"
-  },
-  {
-    path: "/extended-tables",
-    name: "Table List",
-    icon: "files_paper",
-    component: TableList,
-    layout: "/admin"
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "design-2_ruler-pencil",
-    component: Typography,
-    layout: "/admin"
-  },
-  {
-    pro: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "objects_spaceship",
-    component: Upgrade,
+    path: "/history",
+    name: "Historial",
+    icon: "shopping_box",
+    component: Icons,
     layout: "/admin"
   }
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "design_image",
+  //   component: Icons,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/maps",
+  //   name: "Maps",
+  //   icon: "location_map-big",
+  //   component: Maps,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   icon: "ui-1_bell-53",
+  //   component: Notifications,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/user-page",
+  //   name: "User Profile",
+  //   icon: "users_single-02",
+  //   component: UserPage,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/extended-tables",
+  //   name: "Table List",
+  //   icon: "files_paper",
+  //   component: TableList,
+  //   layout: "/admin"
+  // },
+  // {
+  //   path: "/typography",
+  //   name: "Typography",
+  //   icon: "design-2_ruler-pencil",
+  //   component: Typography,
+  //   layout: "/admin"
+  // },
+  // {
+  //   pro: true,
+  //   path: "/upgrade",
+  //   name: "Upgrade to PRO",
+  //   icon: "objects_spaceship",
+  //   component: Upgrade,
+  //   layout: "/admin"
+  // }
 ];
 export default dashRoutes;
